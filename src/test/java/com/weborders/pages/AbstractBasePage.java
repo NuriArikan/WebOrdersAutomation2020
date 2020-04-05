e0e116a7943db7e4d34054bde5ec2a6fbddd396b
NuriArikan <nuri@cornelldecor.com>
2020-04-05 13:53:37 -0400

    first commit

---
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -0,0 +1,24 @@
+package com.weborders.pages;
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Set;
+
+// it mean to be extended
+public abstract class AbstractBasePage {
+
+    protected WebDriver driver = Driver.getDriver();
+
+    protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+
+    public AbstractBasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
